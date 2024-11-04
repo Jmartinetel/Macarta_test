c19c814a2e027df77d88a491f83f4b78637f5777
Jmartinetel <jos-mart12@hotmail.com>
2024-11-03 21:16:52 -0600

    version final

--- src/EDA/01_eda.R
+++ src/EDA/01_eda.R
@@ -1,11 +1,17 @@
 # install.packages('pacman')
 
 
+## Installing packages
+
 pacman::p_load(tidyverse, dplyr, lubridate, ggplot2, scales, ggpubr, patchwork,
                corrplot)
 
 
 
+
+# Import data -------------------------------------------------------------
+
+## We import the data and transform the date column to date
 data <- 
   read.csv("cache/prep_data.csv",nrows = F) %>% 
   mutate(date = as.Date(date))
@@ -19,8 +25,22 @@ data %>%
 
 
 
-# ggsearch analysis --------------------------------------------------------
+# Analysis --------------------------------------------------------
 
+
+#############################################################3
+## Let's start analyzing the behavior of sales against the
+## `lead_n` column.
+##
+## We will focus our main analysis on the `lead_n` variable,
+## as it allows us to identify if a campaign is performing well.
+#########################################################
+
+data %>% 
+  names()
+
+
+## Create a monthly data, and applied some trasnformations to the variables
 monthly_data <- 
   data %>% 
   mutate(date_monthly = as.Date(paste0(year(date), "-", month(date), "-" , "01"))) %>% 
@@ -31,6 +51,8 @@ monthly_data <-
             promotion = sum(promotion),
             leads_n = sum(leads_n), 
             purchase_leads = sum(purchase_leads),
+            purchase_leads_fur = sum(purchase_leads_fur),
+            time_to_purchase = mean(ifelse(time_to_purchase== 0, NA, time_to_purchase), na.rm = T),
             days_considered = mean(ifelse(days_considered== 0, NA, days_considered), na.rm = T),
             days_purchase = mean(ifelse(days_purchase== 0, NA, days_purchase), na.rm = T)
             
@@ -38,6 +60,13 @@ monthly_data <-
 
 monthly_data %>% summary()
 
+
+
+#############################################################3
+## First, we are going to plot share-of-market line plots.
+## The goal is to identify if there is a clear trend or change.
+##
+#########################################################
 monthly_data %>% 
 
   mutate(total = ggsearch+ facebook+ display) %>% 
@@ -66,7 +95,14 @@ monthly_data %>%
         x = "", 
         y = "Daily Spending")
 
+## Is not a clear beahivor in this share plot
 
+
+#############################################################3
+## We are going to rescale the data to compare if there is
+## a trend in the media against the leads.
+##
+#########################################################
 
 
 monthly_data %>% 
@@ -96,6 +132,10 @@ monthly_data %>%
         subtitle = "Leads count",
         x = "", 
         y = "Daily Spending")
+
+#############################################################3
+##Now, we are going to compare the time taken to complete a purchase.
+#########################################################
 
 monthly_data %>% 
   
@@ -128,14 +168,26 @@ monthly_data %>%
 
 # Scatter plots -----------------------------------------------------------------
 
+
+#############################################################3
+## In this section, we are going to plot leads against media spending.
+## We will also plot a linear model (lm) to see if there is a
+## 1-to-1 relationship.
+## Additionally, we will apply log transformations, as some variables
+## have high values, to determine if the log helps achieve a better linear relationship.
+#########################################################
+
 variables <- c("ggsearch",
                "facebook",
                "display")
 scatter_function <- function( .variable, log = TRUE) {
   # browser()
+  
+  ## Fucntion to generate scatterplot
   if (log) {
     data %>% 
       mutate(!!sym(.variable):= log(!!sym(.variable))) %>% 
+      mutate(leads_n = log(leads_n)) %>% 
       ggplot(aes(x = !!sym(.variable), y = leads_n))+
       geom_point() +
       theme_minimal() +
@@ -168,11 +220,15 @@ scatter_function <- function( .variable, log = TRUE) {
   
 p1 <- scatter_function("ggsearch", F)
 
+
+
 p2 <- scatter_function("facebook", F)
 
 p3 <- scatter_function("display", F)
 
 p1 / p2 / p3
+
+## Log plots
 p1_log <- scatter_function("ggsearch", T)
 
 p2_log <- scatter_function("facebook", T)
@@ -184,10 +240,20 @@ p1_log / p2_log / p3_log
 
 
 
+
 # boxplots ----------------------------------------------------------------
+
+#############################################################3
+## For this section, we are going to analyze
+## the distribution of our spending
+## using boxplots.
+#########################################################
+
 
 boxplot_function <- function(.variable) {
   # browser()
+  
+  #### Function to generate boxplots
   data %>% 
     filter(!!sym(.variable)!= 0 ) %>% 
     ggplot(aes(x = !!sym(.variable), y =""))+
@@ -221,6 +287,14 @@ p1/p2
 
 
 # dates -------------------------------------------------------------------
+
+#############################################################
+### Now, we are going to examine seasonal behavior.
+### It's clear that GGsearch and leads have a relationship
+### through months and days.
+###
+#############################################################
+
 
 
 data_months_lead <- 
@@ -322,7 +396,90 @@ p1+p2+p3
 
 
 
+data %>% 
+  mutate(rate_con = purchase_leads_fur/leads_n) %>% 
+  group_by(month) %>% 
+  summarise(rate_con = mean(rate_con),
+            time_to_purchase = mean(time_to_purchase))
+
+monthly_data %>% 
+  mutate(leads_n =rescale(leads_n),
+         ggsearch = rescale(ggsearch ),
+         display = rescale(display ),
+         
+         facebook = rescale(facebook),
+         year = year(date_monthly)) %>% 
+  ggplot(aes(x = date_monthly,group = factor(year))) + 
+  geom_line(aes( y = leads_n), color = "red")+
+  geom_line(aes( y =ggsearch ), color = "blue")+
+  geom_line(aes( y =facebook ), color = "green")+
+  geom_line(aes( y =display ), color = "black")+
+  
+  
+  facet_wrap(~factor(year), scales = "free")+
+  theme_minimal()
+
+
+data_yearly<- 
+  data %>% 
+  mutate(year = year(date)) %>% 
+  group_by(year , month) %>% 
+  summarise(ggsearch = mean(ggsearch), 
+            facebook = mean(facebook),
+            display = mean(display), 
+            promotion = sum(promotion),
+            leads_n = sum(leads_n), 
+            purchase_leads = sum(purchase_leads),
+            days_considered = mean(ifelse(days_considered== 0, NA, days_considered), na.rm = T),
+            days_purchase = mean(ifelse(days_purchase== 0, NA, days_purchase), na.rm = T)) %>% 
+  mutate(month= factor(month, 
+                       levels= c('January','February' ,'March',
+                                 'April','May', 'June', 'July', 
+                                 'August','September', 'October', 'November',
+                                 'December'))) %>% 
+  arrange(month)
+
+
+data_yearly %>% 
+  ggplot(aes(x = month, y = leads_n,group = factor(year), color = factor(year))) + 
+  geom_line()+
+  theme_minimal()
+
+
+
 # corrplot ----------------------------------------------------------------
+
+#############################################################
+### Finally, we are going to examine relationships.
+### An interesting observation is that, although in daily data
+### we see a relationship with days of the week,
+### when we aggregate to monthly data, we observe a stronger correlation
+### among the variables.
+#############################################################
+
 
 cor(data %>% select(where(is.numeric)) %>% select(-X)) %>% 
   corrplot::corrplot(method = 'number')
+
+monthly_data %>% 
+  select(where(is.numeric)) %>% 
+  cor() %>% 
+  corrplot(method = 'number')
+
+
+
+# CONCLUSIONS -------------------------------------------------------------
+#############################################################
+#
+# CONCLUSIONS: We are going to focus the analysis on 
+#`lead_n` (number of leads),
+# but we will develop two models: one using daily data 
+# and the other using weekly data.
+#
+####################################################################
+
+
+
+
+
+
